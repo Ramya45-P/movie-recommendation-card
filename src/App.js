8caa9235275409de415bbb3624680a2fe4107403
Ramya <pramya9912@gmail.com>
2026-05-18 13:58:16 +0530

    Movie recommendation card project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MovieCard from "./components/MovieCard";
+import "./App.css";
 
 function App() {
+  const movie = {
+    title: "Inception",
+    genre: "Sci-Fi / Thriller",
+    rating: "8.8/10",
+    description:
+      "A skilled thief enters dreams to steal secrets and gets a final chance at redemption.",
+    image:
+      "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SY679_.jpg",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Recommendation Card</h1>
+
+      <MovieCard
+        title={movie.title}
+        genre={movie.genre}
+        rating={movie.rating}
+        description={movie.description}
+        image={movie.image}
+      />
     </div>
   );
 }
